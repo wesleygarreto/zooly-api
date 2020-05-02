ba3b425f9c0853a785042143b81d837c119441e3
Wesley Garreto <wesley_garreto@hotmail.com>
2020-05-02 17:40:47 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Zooly
+
+Essa API tem o objetivo de disponibilizar os recursos/end-points necessários
+para o consumo pelas aplicações da camada de vizualização: zooly-web e zooly-mobile.
---
+++ src/main/java/br/com/truvainfo/zoolyapi/ZoolyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.truvainfo.zoolyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoolyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoolyApiApplication.class, args);
+	}
+
+}
