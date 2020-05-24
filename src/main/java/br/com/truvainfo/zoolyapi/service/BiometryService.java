35690b5a720a4996b6335e7e6d0259ecbb438d4a
Wesley Garreto <wesley_garreto@hotmail.com>
2020-05-24 18:55:48 -0300

    Ajustar crud de biometrias

--- src/main/java/br/com/truvainfo/zoolyapi/service/BiometryService.java
+++ src/main/java/br/com/truvainfo/zoolyapi/service/BiometryService.java
@@ -1,32 +1,45 @@
 package br.com.truvainfo.zoolyapi.service;
 
+import br.com.truvainfo.zoolyapi.domain.Biometry;
 import br.com.truvainfo.zoolyapi.domain.dto.BiometryDto;
 import br.com.truvainfo.zoolyapi.domain.mapper.BiometryMapper;
 import br.com.truvainfo.zoolyapi.repository.BiometryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+
+import static java.util.Objects.*;
 
 @Service
 @RequiredArgsConstructor
 public class BiometryService {
 	
+	public static final String MSG_ERROR_BIOMETRY_ID = "msg.error.biometry.id";
+	
 	private final BiometryRepository biometryRepository;
 	private final BiometryMapper biometryMapper;
 	
-	public List<BiometryDto> findAnimalBiometrics(final Integer animalCode) {
-		return biometryMapper.toDtoList(biometryRepository.findBiometricsByAnimalId(animalCode));
+	public List<BiometryDto> findAnimalBiometrics(final Integer animalId) {
+		return biometryMapper.toDtoList(biometryRepository.findBiometricsByAnimalId(animalId));
 	}
 	
 	public void saveBiometry(final BiometryDto biometryDto) {
-		biometryRepository.save(biometryMapper.toEntity(biometryDto));
+		
+		final Biometry biometry = biometryMapper.toEntity(biometryDto);
+		
+		if (isNull(biometry.getCreationDate())) {
+			biometry.setCreationDate(new Date());
+		}
+		
+		biometryRepository.save(biometry);
 	}
 	
-	public void deleteBiometry(final Integer biometryCode) {
-		biometryRepository.delete(biometryRepository.findById(biometryCode)
+	public void deleteBiometry(final Integer biometryId) {
+		biometryRepository.delete(biometryRepository.findById(biometryId)
 		                                            .orElseThrow(() -> new IllegalArgumentException(
-				                                            "The Biometry of id " + biometryCode + " not exists")));
+				                                            MSG_ERROR_BIOMETRY_ID + biometryId)));
 	}
 	
 }
