84ec8f5c5c8c1794f8edaa7a992af0d19ff71203
Angleby Vieira <angleby@gmail.com>
2020-05-24 18:11:33 -0300

    Atualizando scripts db_zooly

--- src/main/resources/db_zooly.sql
+++ src/main/resources/db_zooly.sql
@@ -1,3 +1,6 @@
+-- create schema
+create schema zooly;
+
 -- create new roles
 insert into zooly.user_role values (1, 'ROLE_ADMIN');
 insert into zooly.user_role values (2, 'ROLE_MANAGER');
@@ -8,13 +11,13 @@ select * from zooly.user_role ur;
 
 -- create sample user
 
-insert into zooly.`user` (cd_user, name, email, password, cd_role, active, creation_date) values (1, 'King Arthur', 'kingdom@reign.com', '123', 1, true, CURRENT_TIMESTAMP());
-insert into zooly.`user` (cd_user, name, email, password, cd_role, active, creation_date) values (2, 'Manager', 'manager@zooly.com', '123', 2, true, CURRENT_TIMESTAMP());
-insert into zooly.`user` (cd_user, name, email, password, cd_role, active, creation_date) values (3, 'Doctor', 'doctor@zooly.com', '123', 3, true, CURRENT_TIMESTAMP());
-insert into zooly.`user` (cd_user, name, email, password, cd_role, active, creation_date) values (4, 'Operator', 'operator@zooly.com', '123', 4, true, CURRENT_TIMESTAMP());
+insert into zooly.user (cd_user, name, email, password, cd_role, active, creation_date) values (1, 'King Arthur', 'kingdom@reign.com', '123', 1, true, CURRENT_TIMESTAMP);
+insert into zooly.user (cd_user, name, email, password, cd_role, active, creation_date) values (4, 'Operator', 'operator@zooly.com', '123', 4, true, CURRENT_TIMESTAMP);
+insert into zooly.user (cd_user, name, email, password, cd_role, active, creation_date) values (2, 'Manager', 'manager@zooly.com', '123', 2, true, CURRENT_TIMESTAMP);
+insert into zooly.user (cd_user, name, email, password, cd_role, active, creation_date) values (3, 'Doctor', 'doctor@zooly.com', '123', 3, true, CURRENT_TIMESTAMP);
 
-select * from zooly.`user`;
-delete from zooly.`user`;
+select * from zooly.user;
+delete from zooly.user;
 
 -- delete schema
-drop schema zooly;
+drop schema zooly cascade;
