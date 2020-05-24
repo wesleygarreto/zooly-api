2522d522ea00ddc54969f655bd5ca22c2897a883
Wesley Garreto <wesley_garreto@hotmail.com>
2020-05-23 23:58:35 -0300

    Adicionar métodos de busca na camada de repository

--- src/main/java/br/com/truvainfo/zoolyapi/repository/TaskRepository.java
+++ src/main/java/br/com/truvainfo/zoolyapi/repository/TaskRepository.java
@@ -4,7 +4,12 @@ import br.com.truvainfo.zoolyapi.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-
+	
+	List<Task> findAllByAnimalId(final Integer animalCode);
+	
+	List<Task> findAllByResponsibleUserId(final Integer userCode);
 }
