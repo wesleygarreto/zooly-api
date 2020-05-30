fa9bcef8dec7e2a4ed764b27e58225ddcc4d6b58
Angleby Vieira <angleby@gmail.com>
2020-05-30 02:14:52 -0300

    Ajustando entidades

---
+++ src/main/java/br/com/truvainfo/zoolyapi/domain/Log.java
@@ -0,0 +1,35 @@
+package br.com.truvainfo.zoolyapi.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(schema = "ZOOLY", name = "LOG")
+public class Log implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "CD_LOG")
+	private Integer id;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CD_USER")
+	private User user;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "CREATION_DATE")
+	private Date creationDate;
+	
+}
